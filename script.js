c40f75a572fd7ee980785c9d750686d06639c264
Zafar <mdabuzafar78@gmail.com>
2017-11-02 11:50:32 +0530

    Update offline/online status

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict'
 
 let channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
-let logo, name, description;
+let logo, name, description, counter = 0;
 let result = [];
 
 let getJSON = (type, channel) => {
@@ -9,14 +9,25 @@ let getJSON = (type, channel) => {
     fetch(url).then(function(data){
         return data.json();
     }).then(function(myData){
-        getChannelDetails(myData);
+        type == "channels" ? getChannelDetails(myData) : getStatus(myData);
     });
 }
 
 channels.forEach(function(name){
     getJSON('channels',name);
+    getJSON('streams',name);
 });
 
-function getChannelDetails(data){
+let getChannelDetails = (data) => {
     result.push({"name": data.display_name, "logo":data.logo, "url": data.url, "description":data.status});
 }
+
+let getStatus = (data) => {
+    console.log(typeof(data.stream));
+    counter++;
+    if (data.stream === null) {
+        // result[counter]["description"] = "Offline";
+        // console.log(data.stream,counter);
+    }
+}
+console.log(result);
