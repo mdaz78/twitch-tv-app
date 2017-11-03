29c9f5d9acc9b75d5b9097a870d7beb56a6494c5
Adarsh Lilha <adarshlilha@gmail.com>
2017-11-03 10:43:31 +0530

    Make the script fetch data and store it in an array of objects

--- script.js
+++ script.js
@@ -1,33 +1,26 @@
-'use strict'
-
 let channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
-let logo, name, description, counter = 0;
 let result = [];
 
-let getJSON = (type, channel) => {
-    let url =`https://cors-anywhere.herokuapp.com/https://wind-bow.gomix.me/twitch-api/${type}/${channel}`;
-    fetch(url).then(function(data){
-        return data.json();
-    }).then(function(myData){
-        type == "channels" ? getChannelDetails(myData) : getStatus(myData);
-    });
-}
+let fetchAPI = (type,channel) => {
+    let url = `http://wind-bow.glitch.me/twitch-api/${type}/${channel}`;
+    return fetch(url).then(function(response){
+        return response.json();
+    }).then(function(data){
+        return data;
+    }); 
+};
 
-channels.forEach(function(name){
-    getJSON('channels',name);
-    getJSON('streams',name);
+channels.forEach(function(channel){
+    let channelsData = fetchAPI("channels", channel);
+    channelsData.then(function(apiData){
+        let channelInfo = {"name":apiData.display_name, "logo": apiData.logo, "url": apiData.url, "description":apiData.status};
+        let channelStatus = fetchAPI("streams", channel);
+        channelStatus.then(function(streamData){
+            if (streamData.stream === null){
+                channelInfo.description = "Offline";
+            }
+        });
+        result.push(channelInfo);
+    });
 });
 
-let getChannelDetails = (data) => {
-    result.push({"name": data.display_name, "logo":data.logo, "url": data.url, "description":data.status});
-}
-
-let getStatus = (data) => {
-    console.log(typeof(data.stream));
-    counter++;
-    if (data.stream === null) {
-        // result[counter]["description"] = "Offline";
-        // console.log(data.stream,counter);
-    }
-}
-console.log(result);
